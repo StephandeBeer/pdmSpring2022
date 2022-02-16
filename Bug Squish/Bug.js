7b12415cb790415b6071a0812bee3f2ed6a60cfd
StephandeBeer <74291614+StephandeBeer@users.noreply.github.com>
2022-02-16 00:07:24 -0600

    DIOE

--- Bug Squish/Bug.js
+++ Bug Squish/Bug.js
@@ -1,40 +1,68 @@
-let character0;
+let bug, bugArray; 
+let score = 0;
+let speed = 3;
+let time = 30;
+let rad;
 
 function preload() {
-  spelunkyGuy = loadImage("Beetle.png");
+  Beetle = loadImage("Beetle.png");
 }
 
 function setup() {
   createCanvas(1900, 900);
   imageMode(CENTER);
-
-  bug = new Character(spelunkyGuy, 100, 300, 0);
+  bugArray = [];
+  rad = [PI/2, (3*PI)/2, PI, 0];
 }
-
-function keyPressed() {
-  if(keyCode == RIGHT_ARROW){
-    bug.go(1, 0, PI/2);
-  }
-  else if(keyCode == LEFT_ARROW){
-    bug.go(-1, 0, (3*PI)/2);
-  }
-  else if(keyCode == UP_ARROW){
-    bug.go(0, -1, 0);
-  }
-  else if(keyCode == DOWN_ARROW){
-    bug.go(0, 1, PI);
-  }
-}
-
 function keyReleased() {
   bug.stop();
 }
 
 function draw() {
   background(255, 255, 255);
-  bug.draw();
-}
+  
+  for(let i = 0; i < 30; i++){
+    let temp = random(rad);
+    bugArray.push(new Character(Beetle, random(-50, 1950), random(-50, 950),temp ));
+    bugArray[i].draw();
+  }
 
+  for(let j = 0; j < 30; j++){
+    ang = bugArray[j].Angle();
+    if(ang == PI/2){
+      bugArray[j].go(1, 0);
+    }
+    else if(ang == (3*PI)/2){
+      bugArray[j].go(-1, 0);
+    }
+    else if(ang == 0){
+      bugArray[j].go(0, -1);
+    }
+    else if(ang == PI){
+      bugArray[j].go(0, 1);
+    }
+  } 
+  
+  textSize(36);
+  textFont('Georgia');
+  text("Time: "+time, 50, 50 );
+  text("Score: "+score, 50, 80 );
+  if(frameCount % 60 == 0){
+    if(time > 0){
+      time--;
+    }
+  }
+  if(time<=0){
+    textSize(150);
+    fill('Red');
+    text("GAME OVER", 250, 350 );
+  }
+}
+function mouseClicked() {
+  for(let g = 0; g < 30; g++){
+    bugArray[g].Clicked();
+  }
+}
 class Character{
   constructor(sheet, X, Y, Rotate){
     this.characterSheet = sheet;
@@ -44,37 +72,67 @@ class Character{
     this.tx = X; 
     this.ty = Y;
     this.angle = Rotate;
+    this.dead = false;
+    this.clicked = false;
   }
   draw(){  
-    push();  
+    push(); 
     translate(this.tx, this.ty);
     rotate(this.angle);
 
-    if(this.moveX == 0 && this.moveY == 0){
-      image(this.characterSheet, 0, 0, 150, 150, 122, 0, 122, 122);
+    if(!this.dead){
+      if(this.moveX == 0 && this.moveY == 0){
+        image(this.characterSheet, 0, 0, 124, 124, 122, 0, 122, 122);
+      }
+      else{
+        image(this.characterSheet, 0, 0, 124, 124, 122 * (this.id), 0, 122, 122);
+      }
+      if(frameCount % 2 == 0){
+        this.id = (this.id + 1) % 6;
+      }
+      this.tx += speed * this.moveX;
+      this.ty += speed * this.moveY;
+    }
+    if(this.dead){
+      image(this.characterSheet, 0, 0, 124, 124, 122 * 6, 0, 122, 122);
     }
 
-    else{
-      image(this.characterSheet, 0, 0, 150, 150, 122 * (this.id), 0, 122, 122);
+    if(this.clicked&&(time>0)&&this.dead==false){
+      if(mouseX>=(this.tx -61) && mouseX<=(this.tx +61) && mouseY>=(this.ty -61) && mouseY<=(this.ty +61)){
+        this.dead = true;
+        score=score+1;
+        speed=speed+0.55;        
+      }
+      else{
+        this.clicked = false;
+      }
     }
     
-    if(frameCount % 2 == 0){
-      this.id = (this.id + 1) % 6;
+    if(this.tx>=2000){
+      this.tx=0;
     }
-    this.tx += 2 * this.moveX;
-    this.ty += 2 * this.moveY;
+    if(this.ty>=1000){
+      this.ty=0;
+  }
     pop();
   }
 
-  go(directionX, directionY, degree){
+  go(directionX, directionY){
     this.moveX = directionX;
     this.moveY = directionY;
-    this.angle = degree;
+  }
+
+  Clicked(){
+    this.clicked = true;
+  }
+
+  Angle(){
+    return this.angle;
   }
 
   stop(){
     this.moveX = 0;
     this.moveY = 0;
   }
-  
+
 }
