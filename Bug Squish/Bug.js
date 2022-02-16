f2efd1066efcb48477e8c2e3f4a8d8842731470a
StephandeBeer <74291614+StephandeBeer@users.noreply.github.com>
2022-02-15 20:03:31 -0600

    .l

--- Bug Squish/Bug.js
+++ Bug Squish/Bug.js
@@ -1,81 +1,84 @@
-let mouseColor;
+let character0;
+
+function preload() {
+  spelunkyGuy = loadImage("Beetle.png");
+}
 
 function setup() {
-  createCanvas(1500, 850);
-  background('lightgray');
-  mouseColor = color('lightgray');
+  createCanvas(1900, 900);
+  imageMode(CENTER);
+
+  bug = new Character(spelunkyGuy, 100, 300, 0);
+}
+
+function keyPressed() {
+  if(keyCode == RIGHT_ARROW){
+    bug.go(1);
+    //bug.r(PI/2);
+  }
+  else if(keyCode == LEFT_ARROW){
+    bug.go(-1);
+  }
+  else if(keyCode == UP_ARROW){
+    //bug.r(0);
+  }
+  else if(keyCode == DOWN_ARROW){
+    //bug.r(PI);
+  }
+}
+
+function keyReleased() {
+  bug.stop();
 }
 
 function draw() {
-  // Pallet Colors
-  noStroke();
-  fill('red');
-  square(5, 5, 50);
-  fill('orange');
-  square(5, 60, 50);
-  fill('yellow');
-  square(5, 115, 50);
-  fill('green');
-  square(5, 170, 50);
-  fill('cyan');
-  square(5, 225, 50);
-  fill('blue');
-  square(5, 280, 50);
-  fill('magenta');
-  square(5, 335, 50);
-  fill('brown');
-  square(5, 390, 50);
-  fill('white');
-  square(5, 445, 50);
-  fill('black');
-  square(5, 500, 50); 
+  background(255, 255, 255);
+  bug.draw();
+}
 
-  //Line and stroke color change
-  strokeWeight(10.0);
-  stroke(mouseColor);
-
-  //Color change
-  function mousePressed() {
-    if(mouseX > 5 && mouseX < 55){
-      if(mouseY > 5 && mouseY < 55){
-        mouseColor = color('red');
-      }
-      if(mouseY > 60 && mouseY < 110){
-        mouseColor = color('orange');
-      }
-      if(mouseY > 115 && mouseY < 165){
-        mouseColor = color('yellow');
-      }
-      if(mouseY > 170 && mouseY < 220){
-        mouseColor = color('green');
-      }
-      if(mouseY > 225 && mouseY < 275){
-        mouseColor = color('cyan');
-      }
-      if(mouseY > 280 && mouseY < 330){
-        mouseColor = color('blue');
-      }
-      if(mouseY > 335 && mouseY < 385){
-        mouseColor = color('magenta');
-      }
-      if(mouseY > 390 && mouseY < 440){
-        mouseColor = color('brown');
-      }
-      if(mouseY > 445 && mouseY < 495){
-        mouseColor = color('white');
-      }
-      if(mouseY > 500 && mouseY < 550){
-        mouseColor = color('black');
-      }
-    } 
+class Character{
+  constructor(sheet, X, Y, Rotate){
+    this.characterSheet = sheet;
+    this.id = 0;
+    this.direct = 1;
+    this.move = 0; 
+    this.tx = X; 
+    this.ty = Y;
+    this.angle = Rotate;
   }
+  draw(){  
+    push();  
+    translate(this.tx, this.ty);
+    scale(this.direct, 1);
+    rotate(this.angle);
 
-  // Click / Drag detection
-  if(mouseIsPressed){
-    mousePressed();
-    fill(mouseColor);    
-    if(mouseX > 55 || mouseY > 550 ){
-      line(pmouseX, pmouseY, mouseX, mouseY);
+    if(this.move == 0){
+      image(this.characterSheet, 0, 0, 150, 150, 123, 0, 123, 123);
     }
+
+    else{
+      image(this.characterSheet, 0, 0, 150, 150, 123 * (this.id), 0, 123, 123);
+    }
+    
+    if(frameCount % 30 == 0){
+      this.id = (this.id + 1) % 6;
+    }
+    this.tx += 2 * this.move;
+    pop();
   }
+
+  go(direction){
+    this.move = direction;
+    this.direct = direction;
+    this.id = 3;
+  }
+
+  r(degree){
+    this.angle = degree;
+  }
+
+  stop(){
+    this.move = 0;
+  }
+  
 }
