ff9f3e77746c99a976f35a82572f097b72f2fcae
StephandeBeer <74291614+StephandeBeer@users.noreply.github.com>
2022-02-21 23:33:36 -0600

    Sounds

--- Sampler/js/sketch.js
+++ Sampler/js/sketch.js
@@ -1,8 +1,22 @@
+const { Tone } = require("tone/build/esm/core/Tone");
+
 let soundNew = new Tone.Player('Media/Retro.wav');
+let sounds = new Tone.Players({
+  // 'name' : 'file path' ,
+  //rinse and repeat for all files
+  clap: 'clap.wav',
+  highHat: 'highHat.wav',
+  kick: 'kick.wav',
+  metal: 'metal.wav',
+  snare: 'snare.wav'
+})
 
 function setup() {
   createCanvas(400, 400);
   soundNew.toDestination();
+
+  //for players
+  sounds.toDestination();
 }
 
 function draw() {
@@ -12,4 +26,7 @@ function draw() {
 function keyPressed(){
   soundNew.playbackRate = 0.5;
   soundNew.start();
+
+  //for Players
+  sounds.player('name').start();
 }
