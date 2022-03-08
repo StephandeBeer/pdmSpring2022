505662a0886ddaba887feae119d1aae3b26e338c
StephandeBeer <74291614+StephandeBeer@users.noreply.github.com>
2022-03-07 18:33:53 -0600

    Update sketch.js

--- Sound Synthesis/js/sketch.js
+++ Sound Synthesis/js/sketch.js
@@ -1,4 +1,9 @@
 let feq = 800;
+let lazer = false;
+
+function preload() {
+  trooper = loadImage("trooper.jpg");
+}
 
 const synth = new Tone.Synth({
   "oscillator": {
@@ -12,20 +17,78 @@ const synth = new Tone.Synth({
 }
 });
 
+var bgSynth = new Tone.DuoSynth({"vibratoAmount"  : 0.5 ,
+"vibratoRate"  : 5 ,
+"harmonicity"  : 1.5 ,
+"voice0"  : {
+  "volume"  : -10 ,
+  "portamento"  : 0 ,
+  "oscillator"  : {
+      "type"  : "sine"
+  }  ,
+  "filterEnvelope"  : {
+    "attack"  : 0.01 ,
+    "decay"  : 0 ,
+    "sustain"  : 1 ,
+    "release"  : 0.5
+  }  ,
+  "envelope"  : {
+    "attack"  : 0.01 ,
+    "decay"  : 0 ,
+    "sustain"  : 1 ,
+    "release"  : 0.5
+  }
+}  ,
+"voice1"  : {
+  "volume"  : -20 ,
+  "portamento"  : 0 ,
+  "oscillator"  : {
+      "type"  : "sine"
+  }  ,
+  "filterEnvelope"  : {
+    "attack"  : 0.01 ,
+    "decay"  : 0 ,
+    "sustain"  : 1 ,
+    "release"  : 0.5
+  }  ,
+  "envelope"  : {
+    "attack"  : 0.01 ,
+    "decay"  : 0 ,
+    "sustain"  : 1 ,
+    "release"  : 0.5
+  }
+  }});
+
+
 let gain = new Tone.Gain(0.2).toDestination();
 synth.connect(gain);
 
 
 function setup() {
-  createCanvas(800, 800);
-  //synth.start();
+  createCanvas(800, 800); 
+  background(0);
+ 
 }
 
 function draw() {
-  background(220);
+  image(trooper, 0, 10, 800, 800);
+  textSize(40);
+  textAlign(CENTER, CENTER);
+  fill('white');
+  noStroke();
+  text('Move cursor right to change frequency', 400, 25);
+  stroke('red');
+  strokeWeight(8);
 
   if((frameCount % 25) === 0){
     feq = random(1500, 3000);
+  }
+
+  if(lazer){
+    line(0, 90, 150, 140);
+  }
+  if((frameCount % 45) === 0){
+    lazer = false;
   }
 }
 
@@ -33,6 +96,7 @@ function mouseClicked() {
   Tone.start();
   console.log('pressed');
   synth.triggerAttackRelease(feq, "16n");
+  lazer = true;
   if(mouseX < 50){
     synth.frequency.exponentialRampToValueAtTime(50, '+0.35');
   }
