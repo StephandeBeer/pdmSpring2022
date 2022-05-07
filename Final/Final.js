bd8990c70550a9c4d50006b7c31d1958689773f6
StephandeBeer <74291614+StephandeBeer@users.noreply.github.com>
2022-05-07 17:58:41 -0500

    Update Final.js

--- Final/Final.js
+++ Final/Final.js
@@ -1,14 +1,36 @@
 let block;
-
+let platform;
+let platform2;
+let GRAVITY = 1;
+let JUMP = -15;
+let canJump = false;
 function setup() {
   // put setup code here
   createCanvas(1200, 800);
-  block = createSprite(20, 200, 80, 80);
+  block = createSprite(40, 200, 80, 80);
+  platform = createSprite(500, 500, 1000, 100);
+  platform2 = createSprite(700, 400, 500, 100);
 }
 
 function draw() {
   // put drawing code here
   background('lightgray');
+  block.velocity.y += GRAVITY;
+  canJump = false;
+  if(block.velocity.y > 9){
+    block.velocity.y = 9;
+  }
+  if(block.collide(platform)){
+    block.velocity.x = 0;
+    canJump = true;
+  }
+  if(block.collide(platform2)){
+    block.velocity.x = 0;
+    canJump = true;
+  }
+  if (keyWentDown('space') && canJump){
+    block.velocity.y = JUMP;
+  }
   if(keyDown('d')){
     block.velocity.x = 5;
   }
