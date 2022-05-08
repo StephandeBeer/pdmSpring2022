385d3960b9b3ef93d9731df9dc344201ba1b380f
StephandeBeer <74291614+StephandeBeer@users.noreply.github.com>
2022-05-07 20:29:55 -0500

    Improved

--- Final/Code/Final.js
+++ Final/Code/Final.js
@@ -1,38 +1,47 @@
 let player;
-let platform;
-let platform2;
-let platforms;
+alet platforms;
 let GRAVITY = 1;
 let JUMP = -15;
 let canJump = false;
+let numPlat;
+let xCord = [150, 550, 950];
+let yCord = [500, 450, 500];
+let isThick = [0, 1, 0];
+
+function preload() {
+	thickDirt = loadImage('Art/thickDirt.png');
+	thinDirt = loadImage('Art/thinDirt.png');
+	//bgImg = loadImage('assets/flappy_bg.png');
+}
+
 function createLevel(){
-  platform = createSprite(250, 500, 500, 100);
-  platform.shapeColor = color(0,154,23);
-  platform2 = createSprite(750, 450, 500, 200);
-  platform2.shapeColor = color(0,154,23);
+  for(let i = 0; i < numPlat; i++){
+    let platform = createSprite(xCord[i], yCord[i]);
+    platforms.add(platform);
+    if(isThick[i] === 1){
+      platform.addImage(thickDirt);
+    }
+    else{
+      platform.addImage(thinDirt);
+    }
+  }
 }
 function setup() {
-  // put setup code here
   createCanvas(1200, 800);
   player = createSprite(40, 200, 80, 80);
   player.shapeColor = color(206,32,41);
-  createLevel();
+  numPlat = xCord.length;
   platforms = new Group();
-  platforms.add(platform);
-  platforms.add(platform2);
+  createLevel();
 }
 
 function draw() {
-  // put drawing code here
   background(0,181,226);
   
   camera.position.x = player.position.x + 250;
   camera.zoom = 0.5;
 
   canJump = false;
-  // if(player.velocity.y > 25){d
-  //   player.velocity.y = 25;
-  // }
   if(player.collide(platforms)){
     player.velocity.x = 0;
     canJump = true;
